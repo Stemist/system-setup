924e00af3e2d13237dedd2321183ce768c587b36
Stemist <christian.pearson@stemist.ca>
2019-07-22 22:36:44 -0700

    Initial commit.

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Run from home directory.
+# Wire, Firefox, ProtonVPN, 
+
+echo "Updating cache..."
+sudo apt-get update
+
+echo "Installing common software..."
+sudo apt-get install vim i3 nitrogen git htop
+
+echo "Making directories..."
+mkdir /home/quark/Development
+cd /home/quark/Development
+
+echo "Cloning personal repositories..."
+git clone https://github.com/Stemist/system-setup
+git clone https://github.com/Stemist/personal_automation
+
+echo "Done."
