abcdc561afc3f7e0f1fcf9bcbbd0231eceee38ba
quark <quark@nucleus.telus>
2019-07-27 18:42:56 -0700

    More clones in fedora install script.

--- fedora_install.sh
+++ fedora_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Run from home directory.
-# Wire, Firefox, ProtonVPN, 
+# Run from Development directory.
 
 echo "Updating cache..."
 sudo dnf update
@@ -12,12 +11,14 @@ sudo dnf upgrade -y
 echo "Installing common software..."
 sudo dnf install vim i3 i3status dmenu i3lock xbacklight feh conky nitrogen git htop gufw
 
-echo "Making directories..."
-mkdir /home/quark/Development
+echo "Changing to /home/quark/Development/ directory..."
 cd /home/quark/Development
 
 echo "Cloning personal repositories..."
-git clone https://github.com/Stemist/system-setup
 git clone https://github.com/Stemist/personal_automation
+git clone https://github.com/pachocanadian/sole
+git clone https://github.com/Stemist/Mainframe-Hacker
+git clone https://github.com/Stemist/Wave_Invoice_Automator
+git clone https://github.com/Stemist/BudCalc
 
 echo "Done."
