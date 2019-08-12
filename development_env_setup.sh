83a68cbaf0413b8bc498fda5c4bfcc1c90b0d45a
quark <christian.pearson@stemist.ca>
2019-08-11 19:55:41 -0700

    Seperated install types better. Git-related bash is now own script file.

---
+++ development_env_setup.sh
@@ -0,0 +1,8 @@
+echo "Cloning personal repositories..."
+git clone https://github.com/Stemist/personal_automation
+git clone https://github.com/pachocanadian/sole
+git clone https://github.com/Stemist/Mainframe-Hacker
+git clone https://github.com/Stemist/Wave_Invoice_Automator
+git clone https://github.com/Stemist/BudCalc
+
+echo "Done."
